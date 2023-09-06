ca4f2ad293aa7da298a88a4a5b66a642a6699fca
DrChok <144026526+DrChok@users.noreply.github.com>
2023-09-06 14:32:40 +0800

    Update gesture-handler-multimodel.js

--- Trial/gesture-handler-multimodel.js
+++ Trial/gesture-handler-multimodel.js
@@ -9,77 +9,76 @@ AFRAME.registerComponent("gesture-handler-multimodel", {
     },
   
     init: function () {
-      const obj = document.getElementbyId('cis')
-      obj.handleScale = obj.handleScale.bind(obj);
-      obj.handleRotation = obj.handleRotation.bind(obj);
-      obj.handleModel = obj.handleModel.bind(obj);
+      this.handleScale = this.handleScale.bind(this);
+      this.handleRotation = this.handleRotation.bind(this);
+      this.handleModel = this.handleModel.bind(this);
   
-      obj.isVisible = false;
-      obj.initialScale = obj.el.object3D.scale.clone();
-      obj.scaleFactor = 1;
+      this.isVisible = false;
+      this.initialScale = this.el.object3D.scale.clone();
+      this.scaleFactor = 1;
   
-      obj.addEventListener("markerFound", (e) => {
+      this.el.sceneEl.addEventListener("markerFound", (e) => {
         this.isVisible = true;
       });
   
-      obj.addEventListener("markerLost", (e) => {
+      this.el.sceneEl.addEventListener("markerLost", (e) => {
         this.isVisible = false;
       });
     },
   
     update: function () {
-      if (obj.data.enabled) {
-        obj.addEventListener("onefingermove", obj.handleRotation);
-        obj.addEventListener("twofingermove", obj.handleScale);
-        obj.addEventListener("btnevt", obj.handleModel);
+      if (this.data.enabled) {
+        this.el.sceneEl.addEventListener("onefingermove", this.handleRotation);
+        this.el.sceneEl.addEventListener("twofingermove", this.handleScale);
+        this.el.sceneEl.addEventListener("btnevt", this.handleModel);
 
       } else {
-        obj.removeEventListener("onefingermove", obj.handleRotation);
-        obj.removeEventListener("twofingermove", obj.handleScale);
-        obj.removeEventListener("btnevt", obj.handleModel);
+        this.el.sceneEl.removeEventListener("onefingermove", this.handleRotation);
+        this.el.sceneEl.removeEventListener("twofingermove", this.handleScale);
+        this.el.sceneEl.removeEventListener("btnevt", this.handleModel);
         }
     },
   
     remove: function () {
-      obj.removeEventListener("onefingermove", obj.handleRotation);
-      obj.removeEventListener("twofingermove", obj.handleScale);
-      obj.removeEventListener("btnevt", obj.handleModel);
+      this.el.sceneEl.removeEventListener("onefingermove", this.handleRotation);
+      this.el.sceneEl.removeEventListener("twofingermove", this.handleScale);
+      this.el.sceneEl.removeEventListener("btnevt", this.handleModel);
     },
   
     handleRotation: function (event) {
-      if (obj.isVisible) {
-        obj.object3D.rotation.y +=
-          event.detail.positionChange.x * obj.data.rotationFactor;
-        obj.object3D.rotation.x +=
-          event.detail.positionChange.y * obj.data.rotationFactor;
+      if (this.isVisible) {
+        this.el.object3D.rotation.y +=
+          event.detail.positionChange.x * this.data.rotationFactor;
+        this.el.object3D.rotation.x +=
+          event.detail.positionChange.y * this.data.rotationFactor;
       }
     },
 
     handleScale: function (event) {
-      if (obj.isVisible) {
-        obj.scaleFactor *=
+      if (this.isVisible) {
+        this.scaleFactor *=
           1 + event.detail.spreadChange / event.detail.startSpread;
   
-        obj.scaleFactor = Math.min(
-          Math.max(obj.scaleFactor, obj.data.minScale),
-          obj.data.maxScale
+        this.scaleFactor = Math.min(
+          Math.max(this.scaleFactor, this.data.minScale),
+          this.data.maxScale
         );
   
-        obj.object3D.scale.x = obj.scaleFactor * obj.initialScale.x;
-        obj.object3D.scale.y = obj.scaleFactor * obj.initialScale.y;
-        obj.object3D.scale.z = obj.scaleFactor * obj.initialScale.z;
+        this.el.object3D.scale.x = this.scaleFactor * this.initialScale.x;
+        this.el.object3D.scale.y = this.scaleFactor * this.initialScale.y;
+        this.el.object3D.scale.z = this.scaleFactor * this.initialScale.z;
       }
     },
 
     handleModel: function (event) {
-      if (obj.isVisible) {
-        obj.removeAttribute("gltf-model");
-        obj.setAttribute("gltf-model","#orb" + event.detail.type);
-        obj.object3D.rotation.x = 0;
-        obj.object3D.rotation.y = 0;
-        obj.object3D.scale.x = 0.1;
-        obj.object3D.scale.y = 0.1;
-        obj.object3D.scale.z = 0.1;
+      if (this.isVisible) {
+        this.el.removeAttribute("gltf-model");
+        this.el.setAttribute("gltf-model","#orb" + event.detail.type);
+        this.el.object3D.rotation.x = 0;
+        this.el.object3D.rotation.y = 0;
+        this.el.object3D.scale.x = 0.1;
+        this.el.object3D.scale.y = 0.1;
+        this.el.object3D.scale.z = 0.1;
       }
     },
   });
