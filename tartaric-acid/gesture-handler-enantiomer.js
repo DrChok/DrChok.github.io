f853742925146c94696d5967ddfed2bacb01ad9b
DrChok <chok.yew.keong@ejc.edu.sg>
2023-09-07 20:00:29 +0800

    Update

--- tartaric-acid/gesture-handler-enantiomer.js
+++ tartaric-acid/gesture-handler-enantiomer.js
@@ -4,8 +4,6 @@ AFRAME.registerComponent('gesture-handler-enantiomer', {
     schema: {
         enabled: { default: true },
         rotationFactor: { default: 5 },
-        minScale: { default: 0.3 },
-        maxScale: { default: 8 },
     },
 
     init: function() {
@@ -14,7 +12,7 @@ AFRAME.registerComponent('gesture-handler-enantiomer', {
 
         this.isVisible = false;
 //        this.initialScale = this.el.object3D.scale.clone();
-//        this.scaleFactor = 1;
+        this.scaleFactor = 5;
 
         this.el.sceneEl.addEventListener('markerFound', (e) => {
             this.isVisible = true;
@@ -27,41 +25,34 @@ AFRAME.registerComponent('gesture-handler-enantiomer', {
 
     update: function() {
         if (this.data.enabled) {
-            this.el.sceneEl.addEventListener('onefingermove', this.handleRotation);
-//            this.el.sceneEl.addEventListener('twofingermove', this.handleScale);
+            this.el.sceneEl.addEventListener('onefingermove', this.handleRotationz);
+            this.el.sceneEl.addEventListener('twofingermove', this.handleRotationy);
         } else {
-            this.el.sceneEl.removeEventListener('onefingermove', this.handleRotation);
-//            this.el.sceneEl.removeEventListener('twofingermove', this.handleScale);
+            this.el.sceneEl.removeEventListener('onefingermove', this.handleRotationz);
+//            this.el.sceneEl.removeEventListener('twofingermove', this.handleRotationy);
         }
     },
 
     remove: function() {
-        this.el.sceneEl.removeEventListener('onefingermove', this.handleRotation);
-//        this.el.sceneEl.removeEventListener('twofingermove', this.handleScale);
+        this.el.sceneEl.removeEventListener('onefingermove', this.handleRotationz);
+//        this.el.sceneEl.removeEventListener('twofingermove', this.handleRotationy);
     },
 
-    handleRotation: function(event) {
+    handleRotationz: function(event) {
         if (this.isVisible) {
             this.el.object3D.rotation.z +=
                 event.detail.positionChange.x * this.data.rotationFactor;
-            this.el.object3D.rotation.y +=
-                event.detail.positionChange.y * this.data.rotationFactor;
         }
     },
 
-//    handleScale: function(event) {
-//        if (this.isVisible) {
-//            this.scaleFactor *=
-//                1 + event.detail.spreadChange / event.detail.startSpread;
+    handleRotationy: function(event) {
+        if (this.isVisible) {
+            this.scaleFactor *=
+                1 + event.detail.spreadChange / event.detail.startSpread;
 
-//            this.scaleFactor = Math.min(
-//                Math.max(this.scaleFactor, this.data.minScale),
-//                this.data.maxScale
-//            );
+            this.el.object3D.rotation.y +=
+                event.detail.positionChange.y * this.scaleFactor;
 
-//            this.el.object3D.scale.x = this.scaleFactor * this.initialScale.x;
-//            this.el.object3D.scale.y = this.scaleFactor * this.initialScale.y;
-//            this.el.object3D.scale.z = this.scaleFactor * this.initialScale.z;
-//        }
-//    },
+        }
+    },
 });
